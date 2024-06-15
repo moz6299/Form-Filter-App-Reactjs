e1509ac6b2eeade2e8e24ee4b6d1f723feb74e28
moz6299 <motazhesham585@gmail.com>
2024-06-15 13:32:35 +0300

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Simple-Input-Filter-App-Reactjs/',
+  base: '/Form-Filter-App-Reactjs/',
 });
